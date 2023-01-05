a08270ca62032b78defc1025074a9e9c5e6b47c9
Nina Hawari <nina.hawari@gmail.com>
2023-01-05 22:54:50 +0200

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function Scooter(year,color,model) {
+
+        this.year = year,
+        this.color = color,
+        this.model = model
+
+}
+
+const newScooter = Scooter(2015,"red","sport")
+
+function Driver(name,age,experience) {
+
+       this.name = name,
+        this.age = age,
+        this.experience = experience
+
+}
+
+function PickupLocation(address,city) {
+
+       this.address = address,
+        this.city = city
+
+}
